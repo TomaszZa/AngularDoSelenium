b7ba8eb896cb9eb7e890fa17b8151ad7c49d7f19
Zablocki Tomasz <tomasz.zablocki@capgemini.com>
2015-08-19 13:50:06 +0200

    With 3 point

--- spring-data-jpa/src/main/java/pl/spring/demo/mapper/BookMapper.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/mapper/BookMapper.java
@@ -1,32 +1,38 @@
 package pl.spring.demo.mapper;
 
-import pl.spring.demo.entity.BookEntity;
-import pl.spring.demo.to.BookTo;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+import pl.spring.demo.entity.BookEntity;
+import pl.spring.demo.to.BookTo;
+
 public class BookMapper {
 
-    public static BookTo map(BookEntity bookEntity) {
-        if (bookEntity != null) {
-            return new BookTo(bookEntity.getId(), bookEntity.getTitle(), bookEntity.getAuthors());
-        }
-        return null;
-    }
+	public static BookTo map(BookEntity bookEntity) {
+		if (bookEntity != null) {
 
-    public static BookEntity map(BookTo bookTo) {
-        if (bookTo != null) {
-            return new BookEntity(bookTo.getId(), bookTo.getTitle(), bookTo.getAuthors());
-        }
-        return null;
-    }
+			return new BookTo(bookEntity.getId(), bookEntity.getTitle(), changeListOfAuthorsToString(bookEntity));
+		}
+		return null;
+	}
 
-    public static List<BookTo> map2To(List<BookEntity> bookEntities) {
-        return bookEntities.stream().map(BookMapper::map).collect(Collectors.toList());
-    }
+	public static BookEntity map(BookTo bookTo) {
+		if (bookTo != null) {
+			return new BookEntity(bookTo.getId(), bookTo.getTitle()); // brakuje
+																		// authora
+		}
+		return null;
+	}
 
-    public static List<BookEntity> map2Entity(List<BookTo> bookEntities) {
-        return bookEntities.stream().map(BookMapper::map).collect(Collectors.toList());
-    }
+	public static List<BookTo> map2To(List<BookEntity> bookEntities) {
+		return bookEntities.stream().map(BookMapper::map).collect(Collectors.toList());
+	}
+
+	public static List<BookEntity> map2Entity(List<BookTo> bookEntities) {
+		return bookEntities.stream().map(BookMapper::map).collect(Collectors.toList());
+	}
+
+	private static String changeListOfAuthorsToString(BookEntity bookEntity) {
+		return bookEntity.getAuthors().get(0).getFirstName() + " " + bookEntity.getAuthors().get(0).getLastName();
+	}
 }
